241527009f1451218eedf28aa6625a9ec3965607
AVK Naidu <vamshikrishna.naidu@live.com>
2018-02-15 12:46:29 -0500

    Add project files.

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App2
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void HamburgerMenu_ItemInvoked(object sender, HamburgetMenuItemInvokedEventArgs e)
+        {
+            ObservableCollection<string> Items = new ObservableCollection<string>();
+            for (int i = 0; i < 25; i++)
+            {
+                Items.Add(i.ToString());
+            }
+            myGrid.ItemsSource = Items;
+        }
+    }
+}
